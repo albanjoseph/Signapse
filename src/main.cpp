916d296eb54d05e6be4c369ffd5ccc4db3c93218
Adam Frew <adamfrew1999@gmail.com>
2022-02-04 00:06:44 +0000

    Generating UML diagrams for fake classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
+//! Dog class
+class dog{
+public:
+	int legs;
+	void bark(){
+	}
+};
+
+//! Chihuahua class
+class poodle : public dog{
+public:
+	int hairstyle;
+};
+
 //! The main function
 int main(){
 
