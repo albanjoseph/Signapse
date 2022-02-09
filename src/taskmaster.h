c21810a70c498141249bac80fde98704a66051dc
Adam Frew <adamfrew1999@gmail.com>
2022-02-09 15:34:53 +0000

    Added taskmaster

---
+++ src/taskmaster.h
@@ -0,0 +1,17 @@
+//
+// Created by adam on 09/02/2022.
+//
+
+#ifndef SIGNAPSE_TASKMASTER_H
+#define SIGNAPSE_TASKMASTER_H
+
+class Taskmaster{
+public:
+    Taskmaster();
+    void set_task(char new_task);
+protected:
+    char master_task;
+};
+
+
+#endif //SIGNAPSE_TASKMASTER_H
