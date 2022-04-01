7329d84b00a70114f1a5587a543b5afc6bf29eca
= <rossgardiner24@gmail.com>
2022-04-01 14:16:55 +0100

    restructre gui

---
+++ src/Gui.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ross on 01/04/2022.
+//
+
+#include "Gui.h"
---
+++ src/Gui.h
@@ -0,0 +1,20 @@
+//
+// Created by ross on 01/04/2022.
+//
+
+#ifndef SIGNAPSE_GUI_H
+#define SIGNAPSE_GUI_H
+
+#include "qtGenGui.h"
+
+/*
+ * A class to wrap the QT generated header file and handle GUI functionality
+ */
+class Gui {
+private:
+    QMainWindow widget;
+    Ui_MainWindow ui;
+};
+
+
+#endif //SIGNAPSE_GUI_H
