f9cfe197765db638cfd6386d7d50eb84cc09c9a3
rossGardiner <rossGardiner@users.noreply.github.com>
2022-04-05 18:26:54 +0000

    deploy: a20344fc23406cf69d8b4ddc0c5a5a71f836df5f

--- html/search/functions_3.js
+++ html/search/functions_3.js
@@ -1,7 +1,9 @@
 var searchData=
 [
   ['get_5fprogress_0',['get_progress',['../classProgressBar.html#a35deee5c01490dec232d72d092ac1793',1,'ProgressBar']]],
-  ['getletterfromdigit_1',['getLetterFromDigit',['../classSignapseUtils.html#a1a957a29563a73836631ea7dde6ea65d',1,'SignapseUtils']]],
-  ['getnumber_2',['GetNumber',['../classReel.html#ada98684d0365d5162990c3b6b98bf1f8',1,'Reel']]],
-  ['gui_3',['Gui',['../classGui.html#ab2655dbb6d3a91d7e90cb83dad6c0450',1,'Gui']]]
+  ['getboundingbox_1',['getBoundingBox',['../classCamera.html#af971ae525787584f5b3e51a16f934c6c',1,'Camera']]],
+  ['getletterfromdigit_2',['getLetterFromDigit',['../classSignapseUtils.html#a1a957a29563a73836631ea7dde6ea65d',1,'SignapseUtils']]],
+  ['getnumber_3',['GetNumber',['../classReel.html#ada98684d0365d5162990c3b6b98bf1f8',1,'Reel']]],
+  ['geton_4',['getOn',['../classCamera.html#a3eefea2d6ea67dd7d86b94a0e3a5efc4',1,'Camera']]],
+  ['gui_5',['Gui',['../classGui.html#ab2655dbb6d3a91d7e90cb83dad6c0450',1,'Gui']]]
 ];
