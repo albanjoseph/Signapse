2d44405ab469eefd088f43314021a45d5728bd17
= <rossgardiner24@gmail.com>
2022-04-14 13:22:39 +0100

    adds PreProcessor docstrings, #11

--- src/PreProcessor.h
+++ src/PreProcessor.h
@@ -3,7 +3,7 @@
 #include "PipelineLink.h"
 #include "PreProcessorSettings.h"
 /*!
- * A pipeline element to perform pre-processing required for exection with neural network
+ * A pipeline element to perform pre-processing required for Signapse. Adds region of interest, bounding box rectangle and switches pixel format for neural network.
  */
 class PreProcessor : public PipelineLink{
 public:
