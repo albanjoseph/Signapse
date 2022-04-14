5f484ab7e0288fb8b8337bbc13a548b7b9d87f03
= <rossgardiner24@gmail.com>
2022-04-14 12:53:52 +0100

    adds extra docstrings, #11

--- src/SchedulableLink.h
+++ src/SchedulableLink.h
@@ -9,17 +9,20 @@
 #include "BlockingQueue.h"
 
 
-
+/*!
+ * A class which extends PipelineLink to handle latency-bound stages.
+ * The NextScene function is inherited to add Scenes to an internal BlockingQueue. The pure virtual function ProcessScene must be implemented by derived classes to define Scene processing behaviour. The Run method is called in a separate thread and handles waking up the thread when scenes are available and calling back to the next pipeline element.
+ * BlockingQueue is used as a scheduling mechanism for future extension to multithreading.
+ */
 class SchedulableLink : public PipelineLink{
 public:
     void NextScene(Scene s);
     virtual Scene ProcessScene(Scene s) = 0;
     void Start();
     void Stop();
-    void Enqueue(Scene s);
     bool Available();
-    ~SchedulableLink();
 protected:
+    void Enqueue(Scene s);
     void Run();
     BlockingQueue<Scene> scheduleQueue;
     bool isOn = true;
