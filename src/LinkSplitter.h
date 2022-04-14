a6f71075575dcbdd688041baaf10d8ff126be9a6
= <rossgardiner24@gmail.com>
2022-04-14 13:31:02 +0100

    adds docstrings for LinkSplitter, #11

--- src/LinkSplitter.h
+++ src/LinkSplitter.h
@@ -3,6 +3,10 @@
 
 #include "PipelineLink.h"
 
+/*!
+ * A pipeline element which extends PipelineLink to add a secondary callback. The NextScene function is overridden to pass on the scene reference to both registered callbacks.
+ * N.B. this does not duplicate the scene, merely copies the reference to another element. No extra memory is allocated!
+ */
 class LinkSplitter : public PipelineLink{
 public:
     void NextScene(Scene s);
